b8316e5bb2df3f33edd5cf2305c7d7adc928d017
K9Project <v.kh1992@mail.ru>
2024-07-12 14:01:24 +0400

    Calendar Added

--- PracticWorkMassive.cpp
+++ PracticWorkMassive.cpp
@@ -1,5 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <time.h>
 #include <ctime> 
+
 using namespace std;
 
 int main() {
@@ -18,10 +21,26 @@ int main() {
     for (int i = 0; i < ar; i++) 
         {
         for (int j = 0; j < ar; j++) {
-            cout << array[i][j] << " ";
+            cout << array[i][j] << " " ;
         }
         cout << endl;
     }
 
+    {
+        time_t mytime = time(NULL);
+        struct tm* now = localtime(&mytime);
+        printf("\nDate: %d.%d.%d\n", now->tm_mday, now->tm_mon + 1, now->tm_year + 1900);
+       
+       
+    }
+    {
+
+    }
+   
+    
+    
     return 0;
+
 }
+
+
